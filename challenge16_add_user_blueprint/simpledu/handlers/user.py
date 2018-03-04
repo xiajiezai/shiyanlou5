90442e73c7c963f592b1652243cb7d3b4ed4b37a
xiajiezai <xiajiezai@gmail.com>
2018-03-04 23:17:24 +0800

    Update user.py

--- challenge16_add_user_blueprint/simpledu/handlers/user.py
+++ challenge16_add_user_blueprint/simpledu/handlers/user.py
@@ -7,4 +7,5 @@ def user_index(username):
     user=User.query.filter_by(username=username).first_or_404()
     #using filter:
     #user=User.query.filter(User.username==username).first_or_404()
+    #后面要么跟first，要么跟all，不能没有
     return render_template('user.html',user=user)

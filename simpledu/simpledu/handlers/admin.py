aceec6269a4af8a294d56a58bb3ec9d680293611
xiajiezai <xiajiezai@gmail.com>
2018-03-04 16:05:45 +0800

    update

--- simpledu/simpledu/handlers/admin.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-admin = Blueprint('admin',__name__,url_prefix='/admin')
-
-#because you defined prefix in line 2, here you don't have to use '/admin'
-@admin.route('/')
-def admin_index():
-    return 'admin'
--- simpledu/simpledu/handlers/course.py
+++
@@ -1,2 +0,0 @@
-from flask import Blueprint
-course = Blueprint('course',__name__,url_prefix='/courses')
